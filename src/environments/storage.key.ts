20132d90ac7f9bee611aae67ac0ff7c6e6c65677
Kyoz <banminkyoz@gmail.com>
2021-05-13 14:58:39 +0700

    feat: remove an completely from memory

--- src/environments/storage.key.ts
+++ src/environments/storage.key.ts
@@ -9,9 +9,9 @@ export const STORAGE_NAME_2 = 'AND2.NAME_2';
 export const STORAGE_AVATAR_1 = 'AND2.AVATAR_1';
 export const STORAGE_AVATAR_2 = 'AND2.AVATAR_2';
 
-export const DEFAULT_START_DATE = '2013-09-30';
+export const DEFAULT_START_DATE = '?';
 export const DEFAULT_NAME_1 = 'Tiến Đạt';
-export const DEFAULT_NAME_2 = 'Thu An';
+export const DEFAULT_NAME_2 = '?';
 export const DEFAULT_AVATAR_1 = 'https://www.gravatar.com/avatar/10?d=monsterid&f=y';
 export const DEFAULT_AVATAR_2 = 'https://www.gravatar.com/avatar/15?d=monsterid&f=y';
 export const DEFAULT_SECRET_KEY = 'thesecretkey1997';
